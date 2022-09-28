36a0db7a9ec53e39688fe06b50697abf618b7aac
Jacob <jrmadams02@gmail.com>
2022-09-28 13:38:08 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,33 +1,3 @@
-Assignment 4 - Components
-===
+https://a4-jrma14.glitch.me
 
-Due: October 4th, by 11:59 AM.
-
-For this assignment you will re-implement the client side portion of *either* A2 or A3 using either React or Svelte components. If you choose A3 you only need to use components for the data display / updating; you can leave your login UI as is.
-
-[Svelte Tutorial](https://github.com/cs4241-21a/cs4241-21a.github.io/blob/main/using_svelte.md)  
-[React Tutorial](https://github.com/cs4241-21a/cs4241-21a.github.io/blob/main/using_react.md)  
-
-This project can be implemented on any hosting service (Glitch, DigitalOcean, Heroku etc.), however, you must include all files in your GitHub repo so that the course staff can view them.
-
-Deliverables
----
-
-Do the following to complete this assignment:
-
-1. Implement your project with the above requirements.
-3. Test your project to make sure that when someone goes to your main page on Glitch/Heroku/etc., it displays correctly.
-4. Ensure that your project has the proper naming scheme `a4-firstname-lastname` so we can find it.
-5. Fork this repository and modify the README to the specifications below. Be sure to add *all* project files.
-6. Create and submit a Pull Request to the original repo. Name the pull request using the following template: `a4-firstname-lastname`.
-
-Sample Readme (delete the above when you're ready to submit, and modify the below so with your links and descriptions)
----
-
-## Your Web Application Title
-
-your hosting link e.g. http://a4-charlieroberts.glitch.me
-
-Include a very brief summary of your project here and what you changed / added to assignment #3. Briefly (3–4 sentences) answer the following question: did the new technology improve or hinder the development experience?
-
-Unlike previous assignments, this assignment will be solely graded on whether or not you successfully complete it. Partial credit will be generously given.
+I recreated my todo list from a2 using Svelte. All I changed is the html is now all in svelte, and some of the functions have been simplified because I no longer need to generate my own html as Svelte automatically does the updating. The way I setup my original a2 was a bit weird because of the requirements, so I had to adapt it to use Svelte, which is a little weird still, but it is way cleaner than before. If I had been using Svelte from the beginning it could be even cleaner yet.
---
+++ server.js
@@ -0,0 +1,179 @@
+const http = require('http'),
+  fs = require('fs'),
+  // IMPORTANT: you must run `npm install` in the directory for this assignment
+  // to install the mime library used in the following line of code
+  mime = require('mime'),
+  dir = 'public/',
+  port = 3000
+
+let tasks = []
+
+const server = http.createServer(function (request, response) {
+  if (request.method === 'GET') {
+    handleGet(request, response)
+  } else if (request.method === 'POST') {
+    handlePost(request, response)
+  }
+})
+
+const handleGet = function (request, response) {
+  const filename = dir + request.url.slice(1)
+
+  if (request.url === '/') {
+    sendFile(response, 'public/index.html')
+  } else if (request.url === '/gettasks') {
+    response.writeHead(200, "OK", { 'Content-Type': 'text/plain' })
+    response.end(JSON.stringify(render(tasks)))
+  } else {
+    sendFile(response, filename)
+  }
+}
+
+const handlePost = function (request, response) {
+  if (request.url === '/submit') {
+    handleSubmit(request, response)
+  }
+  if (request.url === '/remove') {
+    handleRemove(request, response)
+  }
+  if (request.url === '/update') {
+    handleUpdate(request, response)
+  }
+}
+
+const handleUpdate = function (request, response) {
+  let dataString = ''
+  request.on('data', function (data) {
+    dataString += data
+  })
+
+  request.on('end', function () {
+    data = JSON.parse(dataString)
+    debugger
+    if (data.taskname.length > 0 && data.dueDate.length > 0 && (!tasks.find(n => n.taskname === data.taskname) || data.taskname === data.oldtaskname)) {
+      let today = new Date();
+      let date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+      data.dateCreated = date
+      let dayMS = 24 * 60 * 60 * 1000
+      let priority = 'low'
+      let parts = data.dueDate.split('-');
+      let dueDate = new Date(parts[0], parts[1] - 1, parts[2]);
+      let timeDiff = dueDate.getTime() - today.getTime()//1-3 high 3-5 medium 6+ low
+      let dayDiff = timeDiff / dayMS
+      if (dayDiff <= 5) {
+        priority = 'medium'
+      }
+      if (dayDiff <= 3) {
+        priority = 'high'
+      }
+      data.priority = priority
+      // console.log(data)
+      tasks = tasks.filter(e => e.taskname !== data.oldtaskname)
+      delete data.oldtaskname
+      tasks.push(data)
+
+      response.writeHead(200, "OK", { 'Content-Type': 'text/plain' })
+      response.end(JSON.stringify(render(tasks)))
+    } else {
+      response.writeHead(200, "Bad Input", { 'Content-Type': 'text/plain' })
+      response.end(JSON.stringify({ message: data.taskname.length === 0 ? 'A name for the task must be provided' : data.dueDate.length > 0 ? 'A task with this name already exists' : 'A date for the task must be provided' }))
+    }
+  })
+}
+
+const handleRemove = function (request, response) {
+  let dataString = ''
+  request.on('data', function (data) {
+    dataString += data
+  })
+
+  request.on('end', function () {
+    // console.log(dataString)
+    data = JSON.parse(dataString)
+    // console.log(data)
+    tasks = tasks.filter(e => e.taskname !== data)
+    response.writeHead(200, "OK", { 'Content-Type': 'text/plain' })
+    response.end(JSON.stringify(render(tasks)))
+  })
+}
+
+const handleSubmit = function (request, response) {
+  let dataString = ''
+  request.on('data', function (data) {
+    dataString += data
+  })
+
+  request.on('end', function () {
+    data = JSON.parse(dataString)
+    if (data.taskname.length > 0 && data.dueDate.length > 0 && !tasks.find(n => n.taskname === data.taskname)) {
+      let today = new Date();
+      let date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+      data.dateCreated = date
+      let dayMS = 24 * 60 * 60 * 1000
+      let priority = 'low'
+      let parts = data.dueDate.split('-');
+      let dueDate = new Date(parts[0], parts[1] - 1, parts[2]);
+      let timeDiff = dueDate.getTime() - today.getTime()//1-3 high 3-5 medium 6+ low
+      let dayDiff = timeDiff / dayMS
+      if (dayDiff <= 5) {
+        priority = 'medium'
+      }
+      if (dayDiff <= 3) {
+        priority = 'high'
+      }
+      data.priority = priority
+      // console.log(data)
+      tasks.push(data)
+
+      response.writeHead(200, "OK", { 'Content-Type': 'text/plain' })
+      response.end(JSON.stringify(render(tasks)))
+    }
+    else {
+      response.writeHead(200, "Bad Input", { 'Content-Type': 'text/plain' })
+      response.end(JSON.stringify({ message: data.taskname.length === 0 ? 'A name for the task must be provided' : data.dueDate.length > 0 ? 'A task with this name already exists' : 'A date for the task must be provided' }))
+    }
+  })
+}
+
+const render = function (data) {
+  return tasks.map(element => {
+    return {
+      html: `<div class="task-card" id="${element.taskname}">
+                <div class="task-block">
+                  <img class="pencil" src="pencil.png"/>
+                  <div class="task-info">
+                    <h2>${element.taskname}</h2>
+                    <p>Due: ${element.dueDate}<p>
+                  </div>
+                  <img class="check" src="check.png"/>
+                </div>
+                <hr>
+              </div>`,
+      ...element
+    }
+  })
+}
+
+const sendFile = function (response, filename) {
+  const type = mime.getType(filename)
+
+  fs.readFile(filename, function (err, content) {
+
+    // if the error = null, then we've loaded the file successfully
+    if (err === null) {
+
+      // status code: https://httpstatuses.com
+      response.writeHeader(200, { 'Content-Type': type })
+      response.end(content)
+
+    } else {
+
+      // file not found, error code 404
+      response.writeHeader(404)
+      response.end('404 Error: File Not Found')
+
+    }
+  })
+}
+
+server.listen(process.env.PORT || port)
